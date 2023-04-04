3ab06313ed8ca8246ec66ce7b40f4938b7e7174a
distepstar <jobsdzihinss@gmail.com>
2023-04-04 10:31:33 -0400

    remove target file

--- rust-file-structure-test/target/debug/deps/rust_file_structure_test-692e87c1d80e992c.d
+++
@@ -1,8 +0,0 @@
-/home/ubuntu/projects/rust/rust-file-structure-test/target/debug/deps/rust_file_structure_test-692e87c1d80e992c.rmeta: src/main.rs src/models/admin_model.rs src/models/user_model.rs src/controller/mod.rs
-
-/home/ubuntu/projects/rust/rust-file-structure-test/target/debug/deps/rust_file_structure_test-692e87c1d80e992c.d: src/main.rs src/models/admin_model.rs src/models/user_model.rs src/controller/mod.rs
-
-src/main.rs:
-src/models/admin_model.rs:
-src/models/user_model.rs:
-src/controller/mod.rs:
--- rust-file-structure-test/target/debug/deps/rust_file_structure_test-918502f526a9f55d.d
+++
@@ -1,8 +0,0 @@
-/home/ubuntu/projects/rust/rust-file-structure-test/target/debug/deps/rust_file_structure_test-918502f526a9f55d: src/main.rs src/models/admin_model.rs src/models/user_model.rs src/controller/mod.rs
-
-/home/ubuntu/projects/rust/rust-file-structure-test/target/debug/deps/rust_file_structure_test-918502f526a9f55d.d: src/main.rs src/models/admin_model.rs src/models/user_model.rs src/controller/mod.rs
-
-src/main.rs:
-src/models/admin_model.rs:
-src/models/user_model.rs:
-src/controller/mod.rs:
